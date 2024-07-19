c7374ff2b9bc08e5073b3621a7fee1e010069316
rotelini989 <rotellini9809@gmail.com>
2024-07-19 15:25:30 +0200

    cxzc

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 import logging
 
 
+
 def run(root_dir, image_format):
 
     logging.basicConfig(level=logging.INFO)
@@ -16,8 +17,15 @@ def run(root_dir, image_format):
     sfm.visualize_points_and_mesh()
 
     
-
+def visualize_only(root_dir, image_format):
+    logging.basicConfig(level=logging.INFO)
+    views = create_views(root_dir, image_format)
+    matches = create_matches(views)
+    K = np.loadtxt(os.path.join(root_dir, 'images', 'K.txt'))
+    sfm = SFM(views, matches, K)
+    sfm.visualize_points_and_mesh()
 
 if __name__ == '__main__':
-    run('data\fountain-P11\images', 'jpg')
-
+    root_dir = 'data\fountain-P11'
+    run(root_dir, 'jpg')
+    #visualize_only(root_dir, 'jpg')
